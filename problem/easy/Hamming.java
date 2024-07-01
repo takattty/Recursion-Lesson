198efb41347d422b04d5f3e2e64dc4227a60f494
takattty <r9r_celtics_t9t@icloud.com>
2024-07-01 22:01:39 +0900

    ハミング問題

---
+++ problem/easy/Hamming.java
@@ -0,0 +1,26 @@
+package problem.easy;
+
+import java.nio.charset.StandardCharsets;
+
+public class Hamming {
+
+    public static void main(String[] args) {
+        System.out.println(hammingDistanceInString("toned","roses"));
+        System.out.println(hammingDistanceInString("toned","toned") );
+        System.out.println(hammingDistanceInString("cats","dogs") );
+        System.out.println(hammingDistanceInString("dogandcat","catanddog") );
+        System.out.println(hammingDistanceInString("AABBCCDD","AAAACCCC") );
+    }
+
+    public static int hammingDistanceInString(String string1, String string2){
+        int count = 0;
+
+        for (int i = 0; i<string1.length(); i++) {
+            if (string1.charAt(i) != (string2.charAt(i))) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
