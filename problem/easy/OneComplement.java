01ae9250b28ef86ab42af70a673315c00d76cba4
takattty <r9r_celtics_t9t@icloud.com>
2024-07-09 17:40:56 +0900

    ビット反転問題 - medium

---
+++ problem/easy/OneComplement.java
@@ -0,0 +1,26 @@
+package problem.easy;
+
+public class OneComplement {
+
+    public static void main(String[] args) {
+        System.out.println(oneComplement("00011100"));
+        System.out.println(oneComplement("10010"));
+        System.out.println(oneComplement("001001"));
+        System.out.println(oneComplement("0111010"));
+        System.out.println(oneComplement("1"));
+    }
+
+    public static String oneComplement(String bits){
+        StringBuilder sb = new StringBuilder();
+
+        for (var s : bits.toCharArray()) {
+            if (s == '0') {
+                sb.append('1');
+            } else {
+                sb.append('0');
+            }
+        }
+
+        return sb.toString();
+    }
+}
