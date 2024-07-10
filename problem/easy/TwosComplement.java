a14f4428e66682ef2327676321b02694bd9b275a
takattty <r9r_celtics_t9t@icloud.com>
2024-07-10 17:53:34 +0900

    2の補数問題 - medium

--- problem/easy/TwosComplement.java
+++ problem/easy/TwosComplement.java
@@ -4,23 +4,45 @@ public class TwosComplement {
 
     public static void main(String[] args) {
         System.out.println(twosComplement("00011100"));
-//        System.out.println(twosComplement("00000000"));
-//        System.out.println(twosComplement("00000010"));
-//        System.out.println(twosComplement("11111111"));
-//        System.out.println(twosComplement("01110101"));
-//        System.out.println(twosComplement("00000001"));
-//        System.out.println(twosComplement("10000000"));
-//        System.out.println(twosComplement("10101010"));
-//        System.out.println(twosComplement("11111110"));
+        System.out.println(twosComplement("00000000"));
+        System.out.println(twosComplement("00000010"));
+        System.out.println(twosComplement("11111111"));
+        System.out.println(twosComplement("01110101"));
+        System.out.println(twosComplement("00000001"));
+        System.out.println(twosComplement("10000000"));
+        System.out.println(twosComplement("10101010"));
+        System.out.println(twosComplement("11111110"));
     }
 
     public static String twosComplement(String bits){
-        var oneComplementString = oneComplement(bits);
-        System.out.println(Integer.parseInt(oneComplementString));
-        return "";
+
+        if (bits.equals("00000000")) return "100000000";
+
+        var oneComplementString = oneComplement(bits).reverse();
+        var result = new StringBuilder();
+        boolean flag = true;
+
+        for (var sb : oneComplementString.toString().toCharArray()) {
+            if (sb == '0') {
+                if (flag) {
+                    result.append("1");
+                    flag = false;
+                } else {
+                    result.append("0");
+                }
+            } else {
+                if (flag) {
+                    result.append("0");
+                } else {
+                    result.append("1");
+                }
+            }
+        }
+
+        return result.reverse().toString();
     }
 
-    private static String oneComplement(String bits) {
+    private static StringBuilder oneComplement(String bits) {
         StringBuilder sb = new StringBuilder();
 
         for (var s : bits.toCharArray()) {
@@ -31,6 +53,6 @@ public class TwosComplement {
             }
         }
 
-        return sb.toString();
+        return sb;
     }
 }
