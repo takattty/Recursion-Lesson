872a1c4aad6707b68124dc44473a5d1d0ec6845e
takattty <r9r_celtics_t9t@icloud.com>
2024-07-17 22:03:46 +0900

    フィボナッチ問題 - easy リファクタ

--- problem/easy/Fibonacci.java
+++ problem/easy/Fibonacci.java
@@ -2,7 +2,14 @@ package problem.easy;
 
 public class Fibonacci {
     public static void main(String[] args) {
-        System.out.println("計算結果：　" + fibonacci(5));
+        System.out.println("計算結果：　" + fibonacci(21000)); //  355243104
+        System.out.println("計算結果：　" + fibonacci(21050)); //  1809583217
+        System.out.println("計算結果：　" + fibonacci(21080)); //  1753817653
+        System.out.println("計算結果：　" + fibonacci(21090)); //  880171992
+        System.out.println("計算結果：　" + fibonacci(21100)); // -866845037
+        System.out.println("計算結果：　" + fibonacci(21200)); // -4590875
+        System.out.println("計算結果：　" + fibonacci(21500)); // -1290876931
+        System.out.println("計算結果：　" + fibonacci(22000)); // -267429733
     }
 
     public static int fibonacci(int n){
