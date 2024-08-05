b379ce0faaf9aa3b19c20d229a58ea99ac034e0c
takattty <r9r_celtics_t9t@icloud.com>
2024-08-05 21:36:08 +0900

    Gravity問題 - easy リファクタリング

--- problem/easy/Gravity.java
+++ problem/easy/Gravity.java
@@ -1,32 +1,39 @@
 package problem.easy;
 
 public class Gravity {
-  private static double earthGravity = 9.8;
-  private static double jupiterGravity = 24.79;
-  private static double mercuryGravity = 3.7;
-  private static double otherGravity = 0;
+    private static double earthGravity = 9.8;
+    private static double jupiterGravity = 24.79;
+    private static double mercuryGravity = 3.7;
+    private static double otherGravity = 0;
 
-  public static int fallingDistance(String planet, int time) {
-    // 関数を完成させてください
-    return calcMerteToMile((1 / 2) * calcGravity(planet) * Math.pow(time, 2));
-  }
-
-  private static double calcGravity(String planet) {
-    if (planet.equals("Earth")) {
-      return earthGravity;
-    } else if (planet.equals("Jupiter")) {
-      return jupiterGravity;
-    } else if (planet.equals("Mercury")) {
-      return mercuryGravity;
-    } else {
-      return otherGravity;
+    public static int fallingDistance(String planet, int time) {
+        // 関数を完成させてください
+        return calcMerteToMile((1 / 2) * calcGravity(planet) * Math.pow(time, 2));
     }
-  }
 
-  private static int calcMerteToMile(double heightOfMeter) {
-    // private staticなメソッド内でprivate staticな変数は使えない？？
-    double converteMeterToMileValue = 0.000621371;
+    private static double calcGravity(String planet) {
+//    if (planet.equals("Earth")) {
+//      return earthGravity;
+//    } else if (planet.equals("Jupiter")) {
+//      return jupiterGravity;
+//    } else if (planet.equals("Mercury")) {
+//      return mercuryGravity;
+//    } else {
+//      return otherGravity;
+//    }
 
-    return (int) Math.floor(heightOfMeter * converteMeterToMileValue);
-  }
+        return switch (planet) {
+            case "Earch" -> earthGravity;
+            case "Jupiter" -> jupiterGravity;
+            case "Mercury" -> mercuryGravity;
+            default -> otherGravity;
+        };
+    }
+
+    private static int calcMerteToMile(double heightOfMeter) {
+        // private staticなメソッド内でprivate staticな変数は使えない？？
+        double converteMeterToMileValue = 0.000621371;
+
+        return (int) Math.floor(heightOfMeter * converteMeterToMileValue);
+    }
 }
