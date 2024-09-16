50e4860f18664a04b6374e1230c3b303874e4283
takattty <r9r_celtics_t9t@icloud.com>
2024-09-16 15:57:37 +0900

    Files問題 - medium リファクタリング

--- problem/medium/files/Files.java
+++ problem/medium/files/Files.java
@@ -1,10 +1,10 @@
 package problem.medium.files;
 
 public class Files {
-    static final int maxVolumeOfFile = 25;
-    static final int convertMbToGb = 1000;
-    static final String fileExtensionMb = "MB";
-    static final String fileExtensionGb = "GB";
+    static final int MAX_VOLUME_OF_FILE = 25;
+    static final int CONVERT_MB_TO_GB = 1000;
+    static final String FILE_EXTENSION_MB = "MB";
+    static final String FILE_EXTENSION_GB = "GB";
 
     String fileName;
     String fileExtension;
@@ -20,8 +20,8 @@ public class Files {
     }
 
     public String getLifetimeBandwidthSize() {
-        var volumeOfMb = (content.length() * maxVolumeOfFile);
-        return volumeOfMb < convertMbToGb ? volumeOfMb + fileExtensionMb : calcMbToGb(volumeOfMb) + fileExtensionGb;
+        var volumeOfMb = (content.length() * MAX_VOLUME_OF_FILE);
+        return volumeOfMb < CONVERT_MB_TO_GB ? volumeOfMb + FILE_EXTENSION_MB : calcMbToGb(volumeOfMb) + FILE_EXTENSION_GB;
     }
 
     public String prependContent(String data) {
@@ -30,13 +30,18 @@ public class Files {
     }
 
     public String addContent(String data, int position) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < content.length(); i++) {
-            if (i == position) {
-                sb.append(data);
-            }
-            sb.append(content.charAt(i));
-        }
+        // 最初書いたやつ
+//        StringBuilder sb = new StringBuilder();
+//        for (int i = 0; i < content.length(); i++) {
+//            if (i == position) {
+//                sb.append(data);
+//            }
+//            sb.append(content.charAt(i));
+//        }
+
+        // リファクタ候補
+        StringBuilder sb = new StringBuilder(content);
+        sb.insert(position, data);
 
         content = sb.toString();
         return content;
@@ -55,6 +60,10 @@ public class Files {
     }
 
     private String calcMbToGb(double volumeOfMb) {
-        return String.valueOf(volumeOfMb / convertMbToGb);
+        // 最初書いたやつ
+//        return String.valueOf(volumeOfMb / CONVERT_MB_TO_GB);
+
+        // リファクタ候補
+        return String.format("%.3f", volumeOfMb / CONVERT_MB_TO_GB);
     }
 }
