d7854b5e14427acad89974ba99b3a45b37967fbb
takattty <r9r_celtics_t9t@icloud.com>
2024-10-23 20:39:03 +0900

    リストインターセクション問題: easy

---
+++ problem/easy/ListIntersection.java
@@ -0,0 +1,25 @@
+package problem.easy;
+
+import java.util.*;
+
+public class ListIntersection {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(listIntersection(new int[]{1, 2, 3, 4, 5, 6}, new int[]{1, 4, 4, 5, 8, 9, 10, 11})));
+    }
+
+    public static int[] listIntersection(int[] targetList, int[] searchList){
+        Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> list = new ArrayList<>();
+
+        for (int target : targetList) {
+            map.put(target, target);
+        }
+
+        for (int i : searchList) {
+            // 参照先がmapのため、O(1)でアクセス可能に
+            if (map.get(i) != null) list.add(i);
+        }
+
+        return list.stream().distinct().sorted().mapToInt(Integer::intValue).toArray();
+    }
+}
