36178337bbd3601d95d4a8d252afa132362065fb
takattty <r9r_celtics_t9t@icloud.com>
2024-10-28 23:39:20 +0900

    インスペクションの確認

--- problem/easy/IntersectionOfArraysRepeats.java
+++ problem/easy/IntersectionOfArraysRepeats.java
@@ -3,7 +3,6 @@ package problem.easy;
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class IntersectionOfArraysRepeats {
     public static void main(String[] args) {
--- problem/easy/PrimesUpToNCount.java
+++ problem/easy/PrimesUpToNCount.java
@@ -2,7 +2,6 @@ package problem.easy;
 
 import java.util.Arrays;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class PrimesUpToNCount {
     public static void main(String[] args) {
