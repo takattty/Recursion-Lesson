5531d1329250802204e36685368d70c47ec20d22
takattty <r9r_celtics_t9t@icloud.com>
2024-11-28 23:49:38 +0900

    リファクタリング

--- app/src/main/java/org/example/problem/medium/HasSameType.java
+++ app/src/main/java/org/example/problem/medium/HasSameType.java
@@ -37,4 +37,32 @@ public class HasSameType {
 
         return true;
     }
+
+    // リファクタ
+//    public static boolean hasSameType(String user1, String user2) {
+//        if (user1 == null || user2 == null || user1.length() != user2.length()) {
+//            return false;
+//        }
+//
+//        Map<Character, Character> charMapping = new HashMap<>();
+//
+//        for (int i = 0; i < user1.length(); i++) {
+    // ここで配列を作る必要を無くしているかつ、Stringへの変換も不要に
+//            char c1 = user1.charAt(i);
+//            char c2 = user2.charAt(i);
+//
+//            Character mappedChar = charMapping.get(c1);
+    // 条件も減らして、スッキリ。二段に分けるのいい。
+//            if (mappedChar == null) {
+//                if (charMapping.containsValue(c2)) {
+//                    return false;
+//                }
+//                charMapping.put(c1, c2);
+//            } else if (mappedChar != c2) {
+//                return false;
+//            }
+//        }
+//
+//        return true;
+//    }
 }
